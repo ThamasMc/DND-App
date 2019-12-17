a754bfc2f545dfe6ddb5baaf8a2e46c5e35f450c
Thamas McGowan <thamas.mcgowan@shadowhealth.com>
2019-12-17 09:50:53 -0500

    Removes swp files and modifies views

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,6 +1,14 @@
 class CharactersController < ApplicationController
+  before_action :authenticate_user!
   def index
-    return redirect_to new_user_session_path if current_user.nil?
     @characters = current_user.characters
+  end
+
+  def new
+    @character = Character.new
+  end
+
+  def char_params
+    params.require(:character).permit(:name, :race, :other_nested_params) #TODO
   end
 end
